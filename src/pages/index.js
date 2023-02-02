27abc56f2c3ccfa8cb70d13a819bec7369133b3a
btbf <b.t.b.f.corp@gmail.com>
2023-02-02 19:26:18 +0900

    description fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="次のステージを目指すカルダノSPOオンラインサロン <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
